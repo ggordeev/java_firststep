7e8dd85bc035d6409e5175deb9199a049d3104a7
Grigoriy Gordeev <user.exe2@gmail.com>
2016-02-29 19:49:02 +0300

    ЗАДАНИЕ 1. Самостоятельные точки.

--- sandbox/src/main/java/ggordeev/sandbox/MyFirstProg.java
+++ sandbox/src/main/java/ggordeev/sandbox/MyFirstProg.java
@@ -6,13 +6,13 @@ public class MyFirstProg {
 
   public static void main(String[] args) {
 
-    Point p = new Point();
-    Point p1 = new Point(1, 2);
-    Point p2 = new Point(4, 7);
+    Point p1 = new Point(1,1);
+    Point p2 = new Point(2,2);
 
-    System.out.println("Точка p1 = (p1.x , p1.y)");
-    System.out.println("Точка p2 = (" + p2.x + " , " + p2.y + ")");
-    System.out.println("Расстояние между точками р1 и р2 =" + p.distance(p1, p2));
+    System.out.println("Точка p1 = " + p1.x + "," + p1.y);
+    System.out.println("Точка p2 = " + p2.x + "," + p2.y);
+    System.out.println("Расстояние между точками p1 и p2 = " + Point.distance(p1,p2));
   }
+
 
 }
