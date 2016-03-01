1c43a44b6173d121dea31265850f284429ba2280
Grigoriy Gordeev <user.exe2@gmail.com>
2016-03-01 15:09:31 +0300

    Подключены зависимости от доп.библиотек

--- sandbox/src/main/java/ggordeev/sandbox/MyFirstProg.java
+++ sandbox/src/main/java/ggordeev/sandbox/MyFirstProg.java
@@ -5,14 +5,26 @@ import java.awt.*;
 public class MyFirstProg {
 
   public static void main(String[] args) {
+    hello("user");
+    hello("grishka");
+    System.out.println("----------------------------------------------");
 
-    Point p1 = new Point(1,1);
-    Point p2 = new Point(2,2);
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Площадь прямоугольника со стороной " + r.a + " и " + r.b + " = " + r.area());
+    System.out.println("----------------------------------------------");
+
+    Point p1 = new Point(4, 3);
+    Point p2 = new Point(3, 7);
 
     System.out.println("Точка p1 = " + p1.x + "," + p1.y);
     System.out.println("Точка p2 = " + p2.x + "," + p2.y);
-    System.out.println("Расстояние между точками p1 и p2 = " + Point.distance(p1,p2));
+    System.out.println("Расстояние между точками p1 и p2 = " + Point.distance(p1, p2));
   }
 
-
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
 }
