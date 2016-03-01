774c5d31cb80fa783e03a984665bfec3fc5a5006
Grigoriy Gordeev <user.exe2@gmail.com>
2016-03-01 15:09:54 +0300

    Написан первый тест Square

---
+++ sandbox/src/test/java/ggordeev/sandbox/SquareTests.java
@@ -0,0 +1,16 @@
+package ggordeev.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by g.gordeev on 01.03.2016.
+ */
+public class SquareTests {
+
+  @Test
+  public void testArea() {
+  Square s = new Square(5);
+    Assert.assertEquals(s.area(), 25.0);
+  }
+}
