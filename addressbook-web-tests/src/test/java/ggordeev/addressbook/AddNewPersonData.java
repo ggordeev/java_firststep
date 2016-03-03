0ab68ab9ae94519adcc5f1fcc5c657462562db0e
Grigoriy Gordeev <user.exe2@gmail.com>
2016-03-03 18:10:16 +0300

    ЗАДАНИЕ 4. Добавлен автотест для создания контактов в адресной книге

---
+++ addressbook-web-tests/src/test/java/ggordeev/addressbook/AddNewPersonData.java
@@ -0,0 +1,49 @@
+package ggordeev.addressbook;
+
+public class AddNewPersonData {
+  private final String firstname;
+  private final String initials;
+  private final String lastname;
+  private final String address;
+  private final String tel;
+  private final String email;
+  private final String year;
+
+  public AddNewPersonData(String firstname, String initials, String lastname, String address, String tel, String email, String year) {
+    this.firstname = firstname;
+    this.initials = initials;
+    this.lastname = lastname;
+    this.address = address;
+    this.tel = tel;
+    this.email = email;
+    this.year = year;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getInitials() {
+    return initials;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getTel() {
+    return tel;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getYear() {
+    return year;
+  }
+}
