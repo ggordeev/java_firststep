0d9d4db8174e6a839ffaabd967381add8a390b10
Grigoriy Gordeev <user.exe2@gmail.com>
2016-03-15 16:30:42 +0300

    Общий базовый класс

--- addressbook-web-tests/src/test/java/ggordeev/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ggordeev/addressbook/appmanager/NavigationHelper.java
@@ -6,14 +6,14 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 /**
  * Created by g.gordeev on 03.03.2016.
  */
-public class NavigationHelper {
+public class NavigationHelper extends HelperBase {
  private FirefoxDriver wd;
 
   public NavigationHelper(FirefoxDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void gotoGroupPage() {
-    wd.findElement(By.linkText("groups")).click();
+    click(By.linkText("groups"));
   }
 }
